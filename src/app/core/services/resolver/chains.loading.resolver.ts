04f570c185efe061b6cb5a04d474940e2158301f
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2023-08-19 12:16:56 +0400

    Update to ANgular 16

--- src/app/core/services/resolver/chains.loading.resolver.ts
+++ src/app/core/services/resolver/chains.loading.resolver.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { SetupService } from '../setup.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChainsLoadingResolverService implements Resolve<any> {
+export class ChainsLoadingResolverService  {
   constructor(
     private setup: SetupService
   ) { }
--- src/app/core/services/resolver/github.loading.resolver.ts
+++ src/app/core/services/resolver/github.loading.resolver.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { SetupService } from '../setup.service';
 
  
 @Injectable({
   providedIn: 'root'
 })
-export class GithubLoadingResolverService implements Resolve<any> {
+export class GithubLoadingResolverService  {
   constructor(
     private setup: SetupService
   ) { }
